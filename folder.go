2fa36bc92425942a98a7e34e912f71424f31be8b
Chun-Hung Tseng <henrybear327@gmail.com>
2023-08-02 17:30:57 +0200

    Refactor the code Un-expose the unnecessary methods Update go mod

--- folder.go
+++ folder.go
@@ -2,12 +2,8 @@ package proton_api_bridge
 
 import (
 	"context"
-	"io"
-	"log"
-	"os"
 	"time"
 
-	"github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/henrybear327/go-proton-api"
 )
 
@@ -62,102 +58,6 @@ func (protonDrive *ProtonDrive) ListDirectory(
 	}
 
 	return ret, nil
-}
-
-func (protonDrive *ProtonDrive) ListDirectoriesRecursively(
-	ctx context.Context,
-	parentNodeKR *crypto.KeyRing,
-	link *proton.Link,
-	download bool,
-	maxDepth, curDepth /* 0-based */ int,
-	excludeRoot bool,
-	pathSoFar string,
-	paths *[]string) error {
-	/*
-		Assumptions:
-		- we only care about the active ones
-	*/
-	if link.State != proton.LinkStateActive {
-		return nil
-	}
-	// log.Println("curDepth", curDepth, "pathSoFar", pathSoFar)
-
-	var currentPath = ""
-
-	if !(excludeRoot && curDepth == 0) {
-		name, err := link.GetName(parentNodeKR, protonDrive.AddrKR)
-		if err != nil {
-			return err
-		}
-
-		currentPath = pathSoFar + "/" + name
-		// log.Println("currentPath", currentPath)
-		if paths != nil {
-			*paths = append(*paths, currentPath)
-		}
-	}
-
-	if download {
-		if protonDrive.Config.DataFolderName == "" {
-			return ErrDataFolderNameIsEmpty
-		}
-
-		if link.Type == proton.LinkTypeFile {
-			log.Println("Downloading", currentPath)
-			defer log.Println("Completes downloading", currentPath)
-
-			reader, _, _, err := protonDrive.DownloadFile(ctx, link, 0)
-			if err != nil {
-				return err
-			}
-
-			byteArray, err := io.ReadAll(reader)
-			if err != nil {
-				return err
-			}
-			err = os.WriteFile("./"+protonDrive.Config.DataFolderName+"/"+currentPath, byteArray, 0777)
-			if err != nil {
-				return err
-			}
-		} else /* folder */ {
-			if !(excludeRoot && curDepth == 0) {
-				// log.Println("Creating folder", currentPath)
-				// defer log.Println("Completes creating folder", currentPath)
-
-				err := os.Mkdir("./"+protonDrive.Config.DataFolderName+"/"+currentPath, 0777)
-				if err != nil {
-					return err
-				}
-			}
-		}
-	}
-
-	if maxDepth == -1 || curDepth < maxDepth {
-		if link.Type == proton.LinkTypeFolder {
-			childrenLinks, err := protonDrive.c.ListChildren(ctx, protonDrive.MainShare.ShareID, link.LinkID, true)
-			if err != nil {
-				return err
-			}
-			// log.Printf("childrenLinks len = %v, %#v", len(childrenLinks), childrenLinks)
-
-			if childrenLinks != nil {
-				// get current node's keyring
-				linkKR, err := link.GetKeyRing(parentNodeKR, protonDrive.AddrKR)
-				if err != nil {
-					return err
-				}
-
-				for _, childLink := range childrenLinks {
-					err = protonDrive.ListDirectoriesRecursively(ctx, linkKR, &childLink, download, maxDepth, curDepth+1, excludeRoot, currentPath, paths)
-					if err != nil {
-						return err
-					}
-				}
-			}
-		}
-	}
-
-	return nil
 }
 
 func (protonDrive *ProtonDrive) CreateNewFolderByID(ctx context.Context, parentLinkID string, folderName string) (string, error) {
@@ -257,7 +157,7 @@ func (protonDrive *ProtonDrive) MoveFileByID(ctx context.Context, srcLinkID, dst
 }
 
 func (protonDrive *ProtonDrive) MoveFile(ctx context.Context, srcLink *proton.Link, dstParentLink *proton.Link, dstName string) error {
-	return protonDrive.MoveFolder(ctx, srcLink, dstParentLink, dstName)
+	return protonDrive.moveLink(ctx, srcLink, dstParentLink, dstName)
 }
 
 func (protonDrive *ProtonDrive) MoveFolderByID(ctx context.Context, srcLinkID, dstParentLinkID, dstName string) error {
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/henrybear327/Proton-API-Bridge
 go 1.18
 
 require (
-	github.com/ProtonMail/gluon v0.17.0
+	github.com/ProtonMail/gluon v0.17.1-0.20230724134000-308be39be96e
 	github.com/ProtonMail/gopenpgp/v2 v2.7.2
-	github.com/henrybear327/go-proton-api v0.0.0-20230725203741-316f1b3227a9
+	github.com/henrybear327/go-proton-api v0.0.0-20230802152927-59db7bb18c64
 	github.com/relvacode/iso8601 v1.3.0
 	golang.org/x/sync v0.3.0
 )
@@ -28,8 +28,8 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
-	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b // indirect
+	golang.org/x/net v0.13.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 )
